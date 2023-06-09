2cfdc1f71da5d96dc2867e011f5591d2f5ea2b2c
Ranieri <ranieri.michel@gmail.com>
2023-06-09 14:17:14 -0300

    Sync trash counter

--- KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
@@ -54,9 +54,13 @@ public class KitchenObject : NetworkBehaviour {
     }
 
     public void DestroySelf() {
-        kitchenObjectParent.ClearKitchenObject();
 
         Destroy(gameObject);
+    }
+
+    public void ClearKitchenObjectOnParent() {
+        kitchenObjectParent.ClearKitchenObject();
+
     }
 
     public bool TryGetPlate(out PlateKitchenObject plateKitchenObject) {
@@ -69,10 +73,11 @@ public class KitchenObject : NetworkBehaviour {
         }
     }
 
-
-
     public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
         KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
     }
 
+    public static void DestroyKitchenObject(KitchenObject kitchenObject) {
+        KitchenGameMultiplayer.Instance.DestroyKitchenObject(kitchenObject);
+    }
 }
