cad8f0db7bd7c088e0e063b2025f8cfeef195d35
Ranieri <ranieri.michel@gmail.com>
2023-06-09 10:11:57 -0300

    Sync pickup objects

--- KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class KitchenObject : MonoBehaviour {
+public class KitchenObject : NetworkBehaviour {
 
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
@@ -28,8 +29,8 @@ public class KitchenObject : MonoBehaviour {
 
         kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
-        transform.localPosition = Vector3.zero;
+        //transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        //transform.localPosition = Vector3.zero;
     }
 
     public IKitchenObjectParent GetKitchenObjectParent() {
@@ -54,14 +55,8 @@ public class KitchenObject : MonoBehaviour {
 
 
 
-    public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-
-        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-        
-        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
-
-        return kitchenObject;
+    public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+        KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
     }
 
 }
--- KitchenChaos_Multiplayer/Assets/Scripts/Player.cs
+++ KitchenChaos_Multiplayer/Assets/Scripts/Player.cs
@@ -182,4 +182,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         return kitchenObject != null;
     }
 
+    public NetworkObject GetNetworkObject() {
+        return NetworkObject;
+    }
+
 }
